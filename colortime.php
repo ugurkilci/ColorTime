78d8c79c10749475da7ab34809352d936a76da3e
Uğur KILCI <ugurbocugu8@gmail.com>
2016-11-01 10:19:40 +0200

    Saat Sıkıntısı Giderildi.
    Hangi saatte olduğu belli olmadığı için gece veya gündüzü algılamıyordu. Son düzenlemelerle bu sorun giderildi. Lütfen sizlerde kendi zaman dilimlerini yazmayı unutmayınız. Aksi halde Tokyo'da ki biri bunu kullanırsa İstanbul saatine göre kullanacaktır.

--- colortime.php
+++ colortime.php
@@ -2,34 +2,34 @@
 	
 	/**
 	 *
-	 * Uður KILCI
+	 * Ugur KILCI
 	 *
 	 * twitter@ugur2nd
 	 * github@ugurkilci
 	 *
-	 * s1.0 | 31.10.2016
+	 * s1.1 | 31.10.2016
 	 *
 	 */
 	
-	function colortime($color1, $color2){ // Daytime, Night
+	date_default_timezone_set('Europe/Istanbul'); // Timezones
+	
+	function colortime($one, $two){ // Daytime, Night
 		
 		$date = date("H"); // Time
 		
-		// Daytime
-		if($date == 7){ echo $color1; }
-		elseif($date == 8){ echo $color1; }
-		elseif($date == 9){ echo $color1; }
-		elseif($date == 10){ echo $color1; }
-		elseif($date == 11){ echo $color1; }
-		elseif($date == 12){ echo $color1; }
-		elseif($date == 13){ echo $color1; }
-		elseif($date == 14){ echo $color1; }
-		elseif($date == 15){ echo $color1; }
-		elseif($date == 16){ echo $color1; }
-		elseif($date == 17){ echo $color1; }
-		elseif($date == 18){ echo $color1; }
-		// Night
-		else{ echo $color2; }
+		if($date == 07){ echo $one; }
+		elseif($date == 08){ echo $one; }
+		elseif($date == 09){ echo $one; }
+		elseif($date == 10){ echo $one; }
+		elseif($date == 11){ echo $one; }
+		elseif($date == 12){ echo $one; }
+		elseif($date == 13){ echo $one; }
+		elseif($date == 14){ echo $one; }
+		elseif($date == 15){ echo $one; }
+		elseif($date == 16){ echo $one; }
+		elseif($date == 17){ echo $one; }
+		elseif($date == 18){ echo $one; }
+		else{ echo $two; }
 		
 	}
 	
